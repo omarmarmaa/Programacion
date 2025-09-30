3d32f4c7b5af087cf47c5e1017e7c1db02a4ddcf
MMO <omarmarouf345@gmail.com>
2025-09-30 20:07:32 +0200

    cuarta modificación

--- src/ejercicio2/Ejercicio1.java
+++
@@ -1,8 +0,0 @@
-package ejercicio2;
-
-public class Ejercicio1 {
-    public static void main(String[] args) {
-        int B = 2;
-        System.out.println((B>=0) ? "positivo"  : "Negativo");
-    }
-}
